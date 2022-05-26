2aeb77a9ae129b4af9ceb1617cd735a0f1d0ef8e
abdulawalemon1 <emondx4@gmail.com>
2022-05-26 23:47:57 +0600

    warnings solved

--- src/Pages/Home/ContactUs.js
+++ src/Pages/Home/ContactUs.js
@@ -4,35 +4,35 @@ const ContactUs = () => {
     return (
         <div className='my-10'>
             <h2 className='text-4xl font-bold text-center mb-5'>Contact Us</h2>
-            <div class="hero min-h-screen bg-base-200">
-                <div class="hero-content flex-col lg:flex-row-reverse">
-                    <div class="text-center lg:text-left">
-                        <img src={contactUs} class="lg:max-w-xl rounded-lg shadow-2xl" alt='blank' />
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <img src={contactUs} className="lg:max-w-xl rounded-lg shadow-2xl" alt='blank' />
 
                     </div>
-                    <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div class="card-body">
-                            <div class="form-control">
-                                <label class="label">
-                                    <span class="label-text">Name</span>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" placeholder="name" class="input input-bordered" />
+                                <input type="text" placeholder="name" className="input input-bordered" />
                             </div>
-                            <div class="form-control">
-                                <label class="label">
-                                    <span class="label-text">Email</span>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" class="input input-bordered" />
+                                <input type="email" placeholder="email" className="input input-bordered" />
                             </div>
-                            <div class="form-control">
-                                <label class="label">
-                                    <span class="label-text">Feedback</span>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Feedback</span>
                                 </label>
-                                <textarea type="text" placeholder="write to us..." class="input input-bordered h-40" />
+                                <textarea type="text" placeholder="write to us..." className="input input-bordered h-40" />
 
                             </div>
-                            <div class="form-control mt-6">
-                                <button class="btn btn-primary">Submit</button>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Submit</button>
                             </div>
                         </div>
                     </div>
--- src/Pages/Home/MeetOurTeam.js
+++ src/Pages/Home/MeetOurTeam.js
@@ -3,49 +3,49 @@ import React from 'react';
 const MeetOurTeam = () => {
     return (
         <div>
-            <div class="px-4 py-8 lg:py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-16" bis_skin_checked="1">
-                <p class="text-center mb-6 text-2xl font-semibold uppercase tracking-tight text-white md:text-3xl">Top Instructors</p>
-                <p class="text-center mb-24 text-base text-white md:text-md">Learn from the best instructors around the globe at Blink</p>
-                <div class="flex items-center space-y-24 lg:space-y-0 flex-col lg:flex-row justify-center" bis_skin_checked="1">
-                    <div class="p-4 relative" bis_skin_checked="1">
-                        <div class="text-center mb-4 absolute -top-16 right-1/2 transform translate-x-1/2" bis_skin_checked="1">
-                            <div class="relative" bis_skin_checked="1">
-                                <img alt="profile" src="https://i.ibb.co/GsZjY1T/Andrei-Neagoie.jpg" class="mx-auto object-cover rounded-lg h-40 w-40  border-4 border-blue-600 dark:border-gray-800" />
+            <div className="px-4 py-8 lg:py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-16" bis_skin_checked="1">
+                <p className="text-center mb-6 text-2xl font-semibold uppercase tracking-tight text-white md:text-3xl">Top Instructors</p>
+                <p className="text-center mb-24 text-base text-white md:text-md">Learn from the best instructors around the globe at Blink</p>
+                <div className="flex items-center space-y-24 lg:space-y-0 flex-col lg:flex-row justify-center" bis_skin_checked="1">
+                    <div className="p-4 relative" bis_skin_checked="1">
+                        <div className="text-center mb-4 absolute -top-16 right-1/2 transform translate-x-1/2" bis_skin_checked="1">
+                            <div className="relative" bis_skin_checked="1">
+                                <img alt="profile" src="https://i.ibb.co/GsZjY1T/Andrei-Neagoie.jpg" className="mx-auto object-cover rounded-lg h-40 w-40  border-4 border-blue-600 dark:border-gray-800" />
                             </div>
                         </div>
-                        <div class="bg-gray-100 dark:bg-gray-800 rounded-lg shadow-lg px-8 py-4 pt-24" bis_skin_checked="1">
-                            <div class="text-center" bis_skin_checked="1">
-                                <p class="text-2xl text-gray-800 dark:text-white">Andrei Neagoie</p>
-                                <p class="text-xl text-gray-500 dark:text-gray-200 font-light">Senior Executive</p>
-                                <p class="text-md text-gray-500 w-60 dark:text-gray-400 mx-auto py-4 font-light">Andrei is the senior executive of some of the of our  technical section.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="p-4 relative" bis_skin_checked="1">
-                        <div class="text-center mb-4 absolute -top-16 right-1/2 transform translate-x-1/2" bis_skin_checked="1">
-                            <div class="relative" bis_skin_checked="1">
-                                <img alt="profile" src="https://i.ibb.co/5GrX8qq/caleb-kingston.jpg" class="mx-auto object-cover rounded-lg h-40 w-40  border-4 border-blue-600 dark:border-gray-800" />
-                            </div>
-                        </div>
-                        <div class="bg-gray-100 dark:bg-gray-800 rounded-lg shadow-lg px-8 py-4 pt-24" bis_skin_checked="1">
-                            <div class="text-center" bis_skin_checked="1">
-                                <p class="text-2xl text-gray-800 dark:text-white">Caleb Kingston</p>
-                                <p class="text-xl text-gray-500 dark:text-gray-200 font-light">CEO </p>
-                                <p class="text-md text-gray-500 w-60 dark:text-gray-400 mx-auto py-4 font-light">Caleb Kingston is the Cheif Executive Officer.</p>
+                        <div className="bg-gray-100 dark:bg-gray-800 rounded-lg shadow-lg px-8 py-4 pt-24" bis_skin_checked="1">
+                            <div className="text-center" bis_skin_checked="1">
+                                <p className="text-2xl text-gray-800 dark:text-white">Andrei Neagoie</p>
+                                <p className="text-xl text-gray-500 dark:text-gray-200 font-light">Senior Executive</p>
+                                <p className="text-md text-gray-500 w-60 dark:text-gray-400 mx-auto py-4 font-light">Andrei is the senior executive of some of the of our  technical section.</p>
                             </div>
                         </div>
                     </div>
-                    <div class="p-4 relative" bis_skin_checked="1">
-                        <div class="text-center mb-4 absolute -top-16 right-1/2 transform translate-x-1/2" bis_skin_checked="1">
-                            <div class="relative" bis_skin_checked="1">
-                                <img alt="profile" src="https://i.ibb.co/b3sxzqN/Angela-Yu.jpg" class="mx-auto object-cover rounded-lg h-40 w-40  border-4 border-blue-600 dark:border-gray-800" />
+                    <div className="p-4 relative" bis_skin_checked="1">
+                        <div className="text-center mb-4 absolute -top-16 right-1/2 transform translate-x-1/2" bis_skin_checked="1">
+                            <div className="relative" bis_skin_checked="1">
+                                <img alt="profile" src="https://i.ibb.co/5GrX8qq/caleb-kingston.jpg" className="mx-auto object-cover rounded-lg h-40 w-40  border-4 border-blue-600 dark:border-gray-800" />
                             </div>
                         </div>
-                        <div class="bg-gray-100 dark:bg-gray-800 rounded-lg shadow-lg px-8 py-4 pt-24" bis_skin_checked="1">
-                            <div class="text-center" bis_skin_checked="1">
-                                <p class="text-2xl text-gray-800 dark:text-white">Angela Yu</p>
-                                <p class="text-xl text-gray-500 dark:text-gray-200 font-light">Head of HR</p>
-                                <p class="text-md text-gray-500 w-60 dark:text-gray-400 mx-auto py-4 font-light">Hi! My name is Angela Yu! I'm the Head of HR with two decades of experience.</p>
+                        <div className="bg-gray-100 dark:bg-gray-800 rounded-lg shadow-lg px-8 py-4 pt-24" bis_skin_checked="1">
+                            <div className="text-center" bis_skin_checked="1">
+                                <p className="text-2xl text-gray-800 dark:text-white">Caleb Kingston</p>
+                                <p className="text-xl text-gray-500 dark:text-gray-200 font-light">CEO </p>
+                                <p className="text-md text-gray-500 w-60 dark:text-gray-400 mx-auto py-4 font-light">Caleb Kingston is the Cheif Executive Officer.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="p-4 relative" bis_skin_checked="1">
+                        <div className="text-center mb-4 absolute -top-16 right-1/2 transform translate-x-1/2" bis_skin_checked="1">
+                            <div className="relative" bis_skin_checked="1">
+                                <img alt="profile" src="https://i.ibb.co/b3sxzqN/Angela-Yu.jpg" className="mx-auto object-cover rounded-lg h-40 w-40  border-4 border-blue-600 dark:border-gray-800" />
+                            </div>
+                        </div>
+                        <div className="bg-gray-100 dark:bg-gray-800 rounded-lg shadow-lg px-8 py-4 pt-24" bis_skin_checked="1">
+                            <div className="text-center" bis_skin_checked="1">
+                                <p className="text-2xl text-gray-800 dark:text-white">Angela Yu</p>
+                                <p className="text-xl text-gray-500 dark:text-gray-200 font-light">Head of HR</p>
+                                <p className="text-md text-gray-500 w-60 dark:text-gray-400 mx-auto py-4 font-light">Hi! My name is Angela Yu! I'm the Head of HR with two decades of experience.</p>
                             </div>
                         </div>
                     </div>
