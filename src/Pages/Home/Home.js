162df2249519ec3d8dc44825ea6e4542f6c04aed
abdulawalemon1 <emondx4@gmail.com>
2022-05-26 23:11:34 +0600

    Our team sextion added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner';
 import BusinessSummary from './BusinessSummary';
 import ContactUs from './ContactUs';
+import MeetOurTeam from './MeetOurTeam';
 import Products from './Products';
 import Reviews from './Reviews';
 
@@ -12,6 +13,7 @@ const Home = () => {
             <Products></Products>
             <BusinessSummary></BusinessSummary>
             <Reviews></Reviews>
+            <MeetOurTeam></MeetOurTeam>
             <ContactUs></ContactUs>
         </div>
     );
